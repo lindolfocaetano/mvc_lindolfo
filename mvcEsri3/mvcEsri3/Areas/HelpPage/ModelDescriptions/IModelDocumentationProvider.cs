b209697fa6c3e601cd4a4b054da46e3793ac6347
lindolfo caetano <lindolfocaetano@hotmail.com>
2016-06-10 13:03:40 -0300

    mvc_geo

---
+++ mvcEsri3/mvcEsri3/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace mvcEsri3.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
